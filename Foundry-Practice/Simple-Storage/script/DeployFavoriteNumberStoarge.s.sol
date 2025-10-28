77de696fc3f896a674ae15c33c0b587ee6fe9082
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-10-28 23:28:12 +0530

    feat: write a deployment script in solidity for FavoriteNumberStorage contract

---
+++ Foundry-Practice/Simple-Storage/script/DeployFavoriteNumberStoarge.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {FavoriteNumberStorage} from "../src/FavoriteNumberStorage.sol";
+
+contract DeployFavoriteNumberStorage is Script {
+    function run() external returns (FavoriteNumberStorage) {
+       vm.startBroadcast();
+       FavoriteNumberStorage favoriteNumberStorage = new FavoriteNumberStorage();
+       vm.stopBroadcast();
+       
+       return favoriteNumberStorage;
+    }
+
+}
+
