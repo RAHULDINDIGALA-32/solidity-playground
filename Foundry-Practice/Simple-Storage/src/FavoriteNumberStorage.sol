88587751b2cf83f52a5be6c46fa4f4afef88a8c1
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-10-28 16:15:46 +0530

    init: initialize & setup the simple-storage foundry project using forge

---
+++ Foundry-Practice/Simple-Storage/src/FavoriteNumberStorage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract FavoriteNumberStorage{
+ bytes32 public author = "rahul dindigala";
+ uint256 favoriteNumber = 3;
+
+ struct Person{
+    string name;
+    uint256 favoriteNumber;
+ }
+
+
+Person[] public peopleList;
+
+mapping(string => uint256) public findFavoriteNumberByName;
+
+ function setMyFavoriteNumber(uint256 _favoriteNumber) public virtual {
+    favoriteNumber = _favoriteNumber;
+ }
+
+function getMyFavoriteNumber() public view returns(uint256){
+    return favoriteNumber;
+}
+
+function addPerson(string memory _name, uint256 _favoriteNumber) public{
+    peopleList.push( Person({ 
+        name: _name, 
+        favoriteNumber: _favoriteNumber
+     }) );
+    findFavoriteNumberByName[_name] = _favoriteNumber;
+}
+
+}
