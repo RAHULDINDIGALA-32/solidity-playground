4dd9d37ee1a974fb2a1a68eb40ae1b493818d41e
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-10-30 00:14:23 +0530

    feat: write the unit tests for all the functionalities in the FundMe contract which improves the test coverage

--- Foundry-Practice/Fund-Me/src/FundMe.sol
+++ Foundry-Practice/Fund-Me/src/FundMe.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error FundMe_NotOwner();
+error FundMe__NotOwner();
 
 contract FundMe {
 
@@ -14,36 +14,36 @@ contract FundMe {
     uint public constant MINIMUM_FUND_IN_USD = 10e18;
 
     // track the sender address with respective fund sent
-    address[] public funders;
-    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    address[] private s_funders;
+    mapping(address funder => uint256 amountFunded) private s_addressToAmountFunded;
 
     // set the Owner
-    address public immutable i_owner;
+    address private immutable i_owner;
 
     // set the price feed address
-    AggregatorV3Interface private priceFeed; 
+    AggregatorV3Interface private s_priceFeed; 
 
     constructor(address priceFeedAddress){
         i_owner = msg.sender;
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
+        s_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     function fund() public payable  {
 
-        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_FUND_IN_USD, "Didn't send enough ETH (minimum fund is 10 USD)");
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_FUND_IN_USD, "Didn't send enough ETH (minimum fund is 10 USD)");
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
     }
 
     function withDrawFunds() public onlyOwner {
       // require(msg.sender == i_owner, "Only owner can withdraw funds!!");
 
-       for(uint256 i=0; i<funders.length; i++){
-        address funder = funders[i];
-        addressToAmountFunded[funder] =0;
+       for(uint256 i=0; i<s_funders.length; i++){
+        address funder = s_funders[i];
+        s_addressToAmountFunded[funder] =0;
        }
 
-       funders = new address[](0);
+       s_funders = new address[](0);
 
        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         
@@ -52,12 +52,12 @@ contract FundMe {
 
     modifier onlyOwner() {
      //  require(msg.sender == i_owner, "Only Owner can withdraw funds!!");
-     if(msg.sender != i_owner) {revert FundMe_NotOwner(); }
+     if(msg.sender != i_owner) {revert FundMe__NotOwner(); }
        _;
     }
 
     function getversion() public view returns (uint256){
-        return priceFeed.version();
+        return s_priceFeed.version();
     }
 
     receive() external payable { 
@@ -67,6 +67,18 @@ contract FundMe {
     fallback() external payable {
         fund();
      }
+
+    function getAddressToAmountFunded(address fundingAddress) external view returns (uint256){
+        return s_addressToAmountFunded[fundingAddress];
+     }
+
+    function getFunder(uint256 index) external view returns (address){
+        return s_funders[index];
+    }
+
+    function getOwner() external view returns(address){
+        return i_owner;
+    }
 
      
 }
--- Foundry-Practice/Fund-Me/test/FundMeTest.t.sol
+++ Foundry-Practice/Fund-Me/test/FundMeTest.t.sol
@@ -9,9 +9,14 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("testUser");
+    uint256 constant SEND_VALUE = 0.5 ether;
+    uint256 constant STARTING_BALANCE = 100 ether;
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function  testMinimumFundIsTenUsd() public view {
@@ -20,12 +25,77 @@ contract FundMeTest is Test {
 
     function testOwnerIsmsgSender() public view {
        // assertEq(fundMe.i_owner(), address(this));
-       assertEq(fundMe.i_owner(), msg.sender);
+       assertEq(fundMe.getOwner(), msg.sender);
     }
 
     function testGetVersion() public view{
         assertEq(fundMe.getversion(), 4);
     }
+    
+    function testFundFailsWithoutEnoughETH()  public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
 
 
+    function testFundUpdatesFunderDataStructure() public funded(){
+        uint256 amountFunded =fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded {
+       address funder  = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }    
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withDrawFunds();
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        // Arrange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMebalance = address(fundMe).balance;
+
+        // Act
+        vm.prank(fundMe.getOwner());
+        fundMe.withDrawFunds();
+
+        // Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMebalance = address(fundMe).balance;
+        
+        assertEq(endingFundMebalance, 0);
+        assertEq(endingOwnerBalance, startingOwnerBalance + startingFundMebalance);
+    }
+
+    function testWithDrawWithMultipleFunders() public {
+       // Arrange
+        uint256 numberOfFunders=10;
+        uint160 startingFunderIndex =1;
+
+        for(uint160 i= startingFunderIndex; i<=numberOfFunders; i++){
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+          }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMebalance = address(fundMe).balance;
+
+        // Act
+        vm.prank(fundMe.getOwner());
+        fundMe.withDrawFunds();
+
+        // Assert
+        assertEq(address(fundMe).balance, 0);
+        assertEq(fundMe.getOwner().balance, startingOwnerBalance + startingFundMebalance);
+    }
 }
