724e0c9a4703bb40cfc85feaa99110b25c9f98ad
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-11-10 15:34:30 +0530

    feat: write the basic proxy contract with a proxy & logic contrats; And write tests to check the calls in same & different interfaces

--- HardHat-Practice/Proxy-Contract/contracts/Proxy.sol
+++ HardHat-Practice/Proxy-Contract/contracts/Proxy.sol
@@ -7,4 +7,38 @@ contract Proxy {
     constructor(address _implementation) {
         implementation = _implementation;
     }
+
+    fallback() external {
+        (bool success, ) = implementation.call(msg.data);
+        require(success);
+    }
+
+    function changeImplementation(address _implementation) external {
+        implementation = _implementation;
+    }
+
+    function changeX(uint _x) external {
+        Logic1(implementation).changeX(_x);
+    }
+}
+
+
+contract Logic1 {
+    uint public x;
+
+    function changeX(uint _x) external {
+        x = _x;
+    }
+}
+
+contract Logic2 {
+    uint public x;
+
+    function changeX(uint _x) external {
+        x = _x*2;
+    }
+
+    function tripleX(uint _x) external {
+        x = _x*3;
+    }
 }
---
+++ HardHat-Practice/Proxy-Contract/test/Proxy.ts
@@ -0,0 +1,46 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+
+import { network } from "hardhat";
+import { Address } from "web3";
+
+describe("Proxy", async function () {
+  const { viem } = await network.connect();
+  const publicClient = await viem.getPublicClient();
+
+  //eth_getStorageAt
+  async function lookupUint(contractAddress: Address, slot: number) {
+    return parseInt(await publicClient.getStorageAt(contractAddress, slot),);
+  }
+
+  it("Should work with upgrades (with same interafce)", async function () {
+     const implementation1 = await viem.deployContract("Logic1");
+    const implementation2 = await viem.deployContract("Logic2");
+    const proxy = await viem.deployContract("Proxy", [implementation1.address]);
+
+    await proxy.write.changeX([42n]);
+    assert.equal(await implementation1.read.x(), 42n);
+
+    await proxy.write.changeImplementation([implementation2.address]);
+    await proxy.write.changeX([100n]);
+    assert.equal(await implementation2.read.x(), 200n);
+  });
+
+  it("Should work with upgrades (with different interface)", async function () {
+    const implementation1 = await viem.deployContract("Logic1");
+    const implementation2 = await viem.deployContract("Logic2");
+    const proxy = await viem.deployContract("Proxy", [implementation1.address]);
+
+    await proxy.write.changeX([7n]);
+    assert.equal(await implementation1.read.x(), 7n);
+
+    await proxy.write.changeImplementation([implementation2.address]);
+
+    const proxyAsLogic2 = viem.getContractAt("Logic2", proxy.address);
+    (await proxyAsLogic2).write.tripleX([10n]);
+    assert.equal(await implementation2.read.x(), 30n);
+
+    
+  });
+
+});
