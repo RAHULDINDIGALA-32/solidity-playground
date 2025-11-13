a80c0626587f24fa015e82360cc5712b37ac3360
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-11-13 17:07:46 +0530

    feat: write a new dynamic-nft contract MoodNFT using the base64 encoding

---
+++ Foundry-Practice/NFTs/src/MoodNFT.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+/**
+ * @title MoodNFT
+ * @author Rahul Dindigala
+ * @dev A simple ERC721 contract that allows users to mint NFTs representing their mood.
+ */
+contract MoodNFT is ERC721 {
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    uint256 private tokenCounter;
+    //mapping(uint256 => string) private tokenIdToUri;
+    string private sadSvgImageUri;
+    string private happySvgImageUri;
+    mapping(uint256 => Mood) private tokenIdToMood;
+
+    constructor(
+        string memory _sadSvgImageUri,
+        string memory _happySvgImageUri
+    ) ERC721("MoodNFT", "MOOD") {
+        tokenCounter = 0;
+        sadSvgImageUri = _sadSvgImageUri;
+        happySvgImageUri = _happySvgImageUri;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, tokenCounter);
+        tokenIdToMood[tokenCounter] = Mood.HAPPY;
+        tokenCounter += 1;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageURI = tokenIdToMood[tokenId] == Mood.HAPPY
+            ? happySvgImageUri
+            : sadSvgImageUri;
+
+        string memory tokenMetadataInBytes = string(
+            abi.encodePacked(
+                '{"name": "',
+                name(),
+                '", "description": "An NFT that reflects the mood of the owner.", "attributes": [{"trait_type": "mood", "value": 100}], "image": "',
+                imageURI,
+                '"}'
+            )
+        );
+
+        string memory tokenMetadata = Base64.encode(
+            bytes(tokenMetadataInBytes)
+        );
+
+        return string(abi.encodePacked(_baseURI(), tokenMetadata));
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+}
