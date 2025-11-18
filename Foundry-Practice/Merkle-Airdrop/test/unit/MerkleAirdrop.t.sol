2ac7a48657162ad2c988a3741a1894fa5fb648ea
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-11-18 12:00:18 +0530

    test: write unit-tests for the Merkle-Airdrop contract

---
+++ Foundry-Practice/Merkle-Airdrop/test/unit/MerkleAirdrop.t.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SomeToken} from "../../src/SomeToken.sol";
+import {MerkleAirdrop} from "../../src/MerkleAirdrop.sol";
+
+contract MerkleAirdropTest is Test {
+    MerkleAirdrop public airdrop;
+    SomeToken public token;
+
+    bytes32 public root = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    bytes32 public proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 public proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public proof = [proofOne, proofTwo];
+
+    uint256 public constant AMOUNT_TO_CLAIM = 25 * 1e18;
+    uint256 public constant AMOUNT_TO_MINT = 5 * AMOUNT_TO_CLAIM;
+    address validUser;
+    uint256 validUserPrivateKey;
+    address randomUser = makeAddr("random-user");
+
+    function setUp() public {
+        token = new SomeToken();
+        airdrop = new MerkleAirdrop(root, token);
+        token.mint(token.owner(), AMOUNT_TO_MINT);
+        token.transfer(address(airdrop), AMOUNT_TO_MINT);
+        (validUser, validUserPrivateKey) = makeAddrAndKey("user");
+    }
+
+    function testValidUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(validUser);
+
+        vm.prank(validUser);
+        airdrop.claim(validUser, AMOUNT_TO_CLAIM, proof);
+
+        console.log("Claimed token amount: ", token.balanceOf(validUser) - startingBalance);
+    }
+
+    function testRevertOnInvalidUserClaim() public {
+        vm.expectRevert(MerkleAirdrop.MerkleAirdrop__InvalidProof.selector);
+        vm.prank(randomUser);
+        airdrop.claim(randomUser, AMOUNT_TO_CLAIM, proof);
+    }
+}
+
