0898fa527f464824ced36522f868cd8f2e877a73
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-11-20 13:26:24 +0530

    feat: write the openzeppelin based governanceToken & governor contracts

---
+++ Foundry-Practice/DAO/src/Box.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private _number;
+
+    event NumberChanged(uint256 number);
+
+    function store(uint256 newNumber) public onlyOwner {
+        _number = newNumber;
+        emit NumberChanged(_number);
+    }
+
+    function getNumber() external view returns (uitn256) {
+        return _number;
+    }
+}
