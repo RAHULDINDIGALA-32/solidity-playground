86984baf6c8af6c410da104f5b6ebb60300fae74
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-12-02 16:48:22 +0530

    feat: integrate & test the uniswapV3 liquidity & flash functionalities

---
+++ Foundry-Practice/UniSwapV3/test/UniswapV3Flash.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IERC20} from "../src/interfaces/IERC20.sol";
+import {IUniswapV3Pool} from "v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+contract UniswapV3Flash {
+    struct FlashCallbackData {
+        uint256 amount0;
+        uint256 amount1;
+        address caller;
+    }
+
+    IUniswapV3Pool private immutable pool;
+    IERC20 private immutable token0;
+    IERC20 private immutable token1;
+
+    error NotPoolContract();
+
+    constructor(address _pool) {
+        pool = IUniswapV3Pool(_pool);
+        token0 = IERC20(pool.token0());
+        token1 = IERC20(pool.token1());
+    }
+
+    function flash(uint256 amount0, uint256 amount1) external {
+        // Task 1 - ABI encode FlashCallbackData
+        bytes memory data = abi.encode(FlashCallbackData({amount0: amount0, amount1: amount1, caller: msg.sender}));
+        // Task 2 - Call IUniswapV3Pool.flash
+        pool.flash(address(this), amount0, amount1, data);
+    }
+
+    function uniswapV3FlashCallback(
+        // Pool fee x amount requested
+        uint256 fee0,
+        uint256 fee1,
+        bytes calldata data
+    )
+        external
+    {
+        // Task 3 - Check msg.sender is pool
+        if (msg.sender != address(pool)) {
+            revert NotPoolContract();
+        }
+        // Task 4 - Decode data into FlashCallbackData
+        FlashCallbackData memory decodedData = abi.decode(data, (FlashCallbackData));
+        // Custom logic (like arbitrage, etc)
+        // Task 5  - Transfer fees from FlashCallbackData.caller
+        if (fee0 > 0) {
+            token0.transferFrom(decodedData.caller, address(this), fee0);
+        }
+        if (fee1 > 0) {
+            token1.transferFrom(decodedData.caller, address(this), fee1);
+        }
+        // Task 6 - Repay pool, amount borrowed + fee
+        if (decodedData.amount0 > 0) {
+            token0.transfer(address(pool), decodedData.amount0 + fee0);
+        }
+        if (decodedData.amount1 > 0) {
+            token1.transfer(address(pool), decodedData.amount1 + fee1);
+        }
+    }
+}
