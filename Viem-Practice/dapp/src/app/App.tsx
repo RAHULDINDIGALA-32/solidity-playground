96928045dc92edac580fb6f9b856c61670e56efe
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-12-10 00:44:07 +0530

    feat: integrate the rainbowkit connectButton component in the nextjs project

--- Viem-Practice/dapp/src/app/App.tsx
+++ Viem-Practice/dapp/src/app/App.tsx
@@ -2,42 +2,44 @@
 
 import '@rainbow-me/rainbowkit/styles.css';
 import {
-  getDefaultConfig,
-  RainbowKitProvider,
+    getDefaultConfig,
+    RainbowKitProvider,
+    darkTheme
 } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  base,
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    sepolia
 } from 'wagmi/chains';
 import {
-  QueryClientProvider,
-  QueryClient,
+    QueryClientProvider,
+    QueryClient,
 } from "@tanstack/react-query";
 
 
 
 const config = getDefaultConfig({
-  appName: 'My RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [mainnet, polygon, optimism, arbitrum, base],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+    ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
 const queryClient = new QueryClient();
-const App = ({children}: {children: React.ReactNode}) => {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          {children}
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+const App = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider modalSize='wide' theme={darkTheme()} initialChain={sepolia}>
+                    {children}
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    );
 };
 
 
